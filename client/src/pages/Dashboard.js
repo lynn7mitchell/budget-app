0a963ad064d0a2ffc6070e0bb227223cc0077974
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-11-03 10:11:32 -0600

    started dashboard sidebar

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export class Dashboard extends Component {
+    // DASHBOARD or SUMMARY
+    //Transactions
+    //Crypto Watch
+    //Money News
     render() {
         return (
             <div>
-                <h1>Dash</h1>
+                <i className="material-icons account-circle">account_circle</i>
+                <Row>
+                    <Col xs={2} className="dashboard-sidebar">
+                        <ul className="sidebar-items">
+                            <li>Dashboard</li>
+                            <li>Tansactions</li>
+                            <li>Crypto Watch</li>
+                        </ul>
+                    </Col>
+                    <Col xs={10}>
+                    <h1>Dashboard</h1>
+                    </Col>
+                </Row>
             </div>
         )
     }
