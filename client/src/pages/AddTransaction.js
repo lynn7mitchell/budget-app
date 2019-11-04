9e6894824517def1423f8de2528a4fa323003f39
lynn7mitchell <lynn7mitchell@gmail.com>
2019-11-04 17:12:18 -0600

    added Transaction component

---
+++ client/src/pages/AddTransaction.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Table from "react-bootstrap/Table"
+import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
+import Form from "react-bootstrap/Form"
+
+export class AddTransaction extends Component {
+    
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AddTransaction
