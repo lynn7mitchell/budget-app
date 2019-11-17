60a21749671d49b7c9dd1cb1abfae263f29c3ef8
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-11-17 10:01:09 -0600

    SetupBudgets page grabs the user data

--- client/src/pages/SetupBudgets.js
+++ client/src/pages/SetupBudgets.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
+import axios from "axios";
+import setAuthToken from "../utils/setAuthtoken";
 
 export class SetupBudgets extends Component {
+    state = {
+        user: {},
+        redirect: false
+      };
+      componentWillMount() {
+        const token = localStorage.getItem("example-app");
+    
+        if (token) {
+          setAuthToken(token);
+        }
+    
+        axios
+          .get("api/user")
+          .then(response => {
+            this.setState({
+              user: response.data
+            });
+          })
+          .catch(err => console.log(err.response));
+      }
     render() {
         return (
             <div>
